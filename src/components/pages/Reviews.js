38e142a6c5cdfd3e1ab5e3841215e91cd0212e02
Isaac Webber <isaacjwebber@gmail.com>
2022-06-03 19:43:33 +0100

    added proper reviews

--- src/components/pages/Reviews.js
+++ src/components/pages/Reviews.js
@@ -5,9 +5,10 @@ import imgCollab from '../../assets/review-imgs/everycollab.png'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import reviews from '../reviews/reviews';
+
 // TODO add quotes
 // TODO try and center
-// TODO 
 
 function Testimonial(props) {
     const active = props.active != null ? props.active : false;
@@ -26,49 +27,11 @@ function MyCarousel() {
         <div className="testimonials">
             <div class="carousel-wrapper">
                 <Carousel infiniteLoop useKeyboardArrows autoPlay interval={5000} transitionTime={500} showStatus={false} showIndicators={false} centerMode={true} showThumbs={false}>
-                    <Testimonial 
-                                active={true} 
-                                text="When I was running a large remote recording project, Hugh was a great help to bounce ideas off. He has a wealth of songwriting, recording, and arrangement experience." 
-                                author="BLUEBEARD">
-                    </Testimonial>
-
-                    <Testimonial 
-                                text="“Louie was an excellent collaborator,
-                                and a treat to work with. Both imaginative and flexible,
-                                his approach to composition was one that resulted in high quality work,
-                                delivered both consistently and with the professionalism you would expect from an industry veteran.”" 
-                                author="- Chris Pearson, Founder: Tamalpais Games | 2020">
-                    </Testimonial>
-                    
-                    <Testimonial 
-                                text="“He has such a thirst for learning,
-                                and both the range and depth of Louie’s knowledge of the subject are truly impressive.
-                                He is a gifted composer [and] has the true musician’s grasp of the necessity to write idiomatically for instruments
-                                as was evident in the premier of his Piano Trio (piano, violin, cello) movement,
-                                given a rapturous reception by the audience in a concert earlier this term (summer 2018)”" 
-                                author="- Andrew Painter, Head of Music, Lytchett Minster School and 6th Form | 2018">
-                    </Testimonial>
-
-                    <Testimonial 
-                                text="“Louie is talented, flexible, and clearly loves his work. He is a real joy to collaborate with.”" 
-                                author="- Paul Herve, Founder: Rocket Ship Studios | 2020">
-                    </Testimonial>
-
-                    <Testimonial 
-                                text="“The music Louie Taylor creates for our shows is outrageously good. 
-                                We love how he delivers to such a high standard across so many different musical genres. 
-                                I'm still humming his theme tunes now. 
-                                We also really appreciate Louie's swift and professional turnaround of audio materials 
-                                and his keen ability to discuss a project from outset to final edit. It's a real joy working with him.”" 
-                                author="- Neil Mossey, Producer, CoComelon Storytime Spotify Podcast | 2022">
-                    </Testimonial>
-
-                    <Testimonial 
-                                text="“Louie is a collaborative, creative composer and a joy to work with.  
-                                He responds well to feedback and is always keen to develop and 
-                                evolve his music so it’s perfectly tailored for each production.”" 
-                                author="- Geoff Coward, Director, Coding with QMO and BBC | 2022">
-                    </Testimonial>
+                    {reviews.map((item, index) => <Testimonial 
+                                                        active={index === 0 ? true : false}
+                                                        text={item.text}
+                                                        author={item.author}
+                                                        ></Testimonial>)}
                 </Carousel>
             </div></div>
     );
--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,4 +1,9 @@
-export default [
+const reviews = [
     {"text": "When I was running a large remote recording project, Hugh was a great help to bounce ideas off. He has a wealth of songwriting, recording, and arrangement experience.", "author": "Bluebeard"},
-    
+    {"text": "I have had the pleasure of knowing & working with Hugh; he is one of the most talented young writers I've encountered. His versatility, integrity and work ethic are sure to insure him a bright future.", "author": "MICHAEL GARVIN, GRAMMY NOMINEE & MULTI PLATINUM WRITER "},
+    {"text": "I really enjoyed being able to have open and honest discussions about where I am at as an artist. It's allowed me to think more deeply about myself as a songwriter and what my music means to me.", "author": "LINDSEY FARROW"},
+    {"text": "I started with a vague 'feeling' of what I wanted to create. Hugh helped me dig deeper into what I was trying to say, instinctively knowing when there was more to draw out.", "author": "JOANNE"},
+    {"text": "I've worked with Hugh on a number of occasions and am happy to vouch for his talent as a composer and lyricist. He is also a proficient producer and knows how to make a song sound great.", "author": "DOMINIC KING, MULTI PLATINUM SONGWRITER"},
 ]
+
+export default reviews;
