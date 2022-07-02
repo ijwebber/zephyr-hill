2575069e91694c8bbe56f8dbebceecfa4dabbf72
Isaac Webber <isaacjwebber@gmail.com>
2022-07-02 10:22:39 +0100

    started work on mobile

---
+++ src/components/mobilePages/Blog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Blog.css';
+
+function Blog() {
+    return (
+        <div className="mobileBlog">
+            <div className="title">BLOG</div>
+            <div className="button">
+                <a href="https://medium.com/@zephyrhillmusic" target="_blank" rel="noreferrer noopener">MORE POSTS</a>
+            </div>
+        </div>
+    );
+}
+
+export default Blog;
