70a5649aa123bcdceff0e3ae02a5ecd1e57c072d
Isaac Webber <isaacjwebber@gmail.com>
2022-08-20 12:21:48 +0100

    added latest song to both mobile and desktop home pages

--- src/components/browserPages/Home.js
+++ src/components/browserPages/Home.js
@@ -2,12 +2,15 @@ import './Home.css';
 import React from 'react';
 import imgLogo from '../../assets/home-imgs/ZHMLogo.png';
 import imgSoundBetter from '../../assets/home-imgs/SoundBetter.svg';
+import sliderImages from '../../assets/slider-imgs/images';
 
 function Home(props) {
     const navbarOptions = ["MUSIC", "REVIEWS", "CONTACT", "BLOG"]
     const getHeight = () => document.getElementsByName("Page")[0].offsetHeight;
     const scrollTo = index => {window.scroll({top: getHeight() * (index + 1), left: 0, behavior: 'smooth' })};
     const navbarObjs = navbarOptions.map((item, index) => <p key={"navbar" + index} className='option' onClick={() => scrollTo(index)}>{item}</p>)
+
+    const latestSong = sliderImages[0]
 
     return (
         <div className='Home'>
@@ -20,7 +23,13 @@ function Home(props) {
                 <div className='about'>
                     {"I'm a songwriter & creative collaborator who loves writing great songs with great singers".toUpperCase()}
                 </div>
-                <a href='https://soundbetter.com/profiles/122517-zephyrhillmusic' target="_blank" rel="noreferrer noopener"><img src={imgSoundBetter} className="soundbetter" alt="Soundbetter Link"></img></a>
+                <div>
+                    <a href='https://soundbetter.com/profiles/122517-zephyrhillmusic' target="_blank" rel="noreferrer noopener"><img src={imgSoundBetter} className="soundbetter" alt="Soundbetter Link"></img></a>
+                    <div className='latest-song'>
+                        <a href={"https://open.spotify.com/track/"+ latestSong.embed} target="_blank" rel="noreferrer noopener"><img src={latestSong.image}></img></a>
+                        <p>LATEST SONG</p>
+                    </div>
+                </div>
             </div> 
         </div>
     );
--- src/components/mobilePages/Home.js
+++ src/components/mobilePages/Home.js
@@ -2,10 +2,12 @@ import './Home.css';
 import React from 'react';
 import imgLogo from '../../assets/home-imgs/ZHMLogo.png';
 import imgSoundBetter from '../../assets/home-imgs/SoundBetter.svg';
-
+import sliderImages from '../../assets/slider-imgs/images';
 
 
 function Home(props) {
+    const latestSong = sliderImages[0]
+
     return (
         <div className='mobileHome'>
             <div className='container'>
@@ -13,7 +15,13 @@ function Home(props) {
                 <div className='about'>
                     {"I'm a songwriter & creative collaborator who loves writing great songs with great singers".toUpperCase()}
                 </div>
+                <div>
                 <a href='https://soundbetter.com/profiles/122517-zephyrhillmusic' target="_blank" rel="noreferrer noopener"><img src={imgSoundBetter} className="soundbetter" alt="Soundbetter Link"></img></a>
+                    <div className='latest-song'>
+                            <a href={"https://open.spotify.com/track/"+ latestSong.embed} target="_blank" rel="noreferrer noopener"><img src={latestSong.image}></img></a>
+                            <p>LATEST SONG</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
